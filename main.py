64b7549de2b410d988a4f0208ad527ebd77d920c
manan-hemani <mananhemani01@gmail.com>
2025-04-12 17:53:14 +0530

    Updated main.py according to new changes in project
    added condition to check if user exist or not and according to that further steps will work

--- main.py
+++ main.py
@@ -1,52 +1,75 @@
+#create 2 csv files
+#1) User.csv
+#2) Reccomdation.csv
+
 import pandas as pd
+import numpy as np
+import matplotlib as mb
 
 #starting title of the project
 print("Welcome to academic Tracker and Resource Recommander\n\n")
 print("*"*30)
 
-#loop for inputting the details
-while True:
-  print("Please Input the Necessary Details to Track your Progress:\n\n")
+#ask user to enter name for checking there previous data
+s_name=input("Your Name:")
 
-#Inputting the details of the user
-  s_name=input("Your Name:")
-  no_of_sub=int(input("Enter the No.of.Subject:"))
+#condition to check user
+if(s_name=="abc"): #need to change condition for csv file to check name
+  print("User Exist\nWelcome Back :)\n Displaying your data")
+  #csv code to display data
 
-  scores = {} #using dictionary for easy insertion in csv  
-
-# using loop to add all subjects marks
-  for i in range(no_of_sub):
-    subject_name=input(f"Enter Subject {i+1}: ")
-    score_got = int(input(f"Enter the mark you scored in {subject_name}: "))
+else:
+  print("User Not Exist :(\nlease Enter the Necessary Details to Track your Progress:\n\n")
+  
+  #loop for inputting the details and helping in further checking
+  
+  while True:
+    scores = {} #using dictionary for insertion in csv  
+  
+  #Inputting the details of the user
+    #no need for re-enter name will take from above
     
-    #adding values in dictionary
-    scores[subject_name]=score_got  
+    no_of_sub=int(input("Enter the No.of.Subject:"))
+    
+  # using loop to add all subjects marks
+    for i in range(no_of_sub):
+      subject_name=input(f"Enter Subject {i+1}: ")
+      score_got = int(input(f"Enter the marks you scored in {subject_name}: "))
+      
+      #adding values in dictionary
+      scores[subject_name]=score_got  
 
-  attendance=float(input("Enter your Overall Attendance Percentage:"))
+    attendance=float(input("Enter your Overall Attendance Percentage:"))
 
-#printing the inputted details for confirmation
-#for confirmation show it in the table form
-  data = []
-  for subject, score in scores.items():
-      data.append({
-          'Student Name': s_name,
-          'Subject': subject,
-          'Score': score,
-          'Attendance (%)': attendance
-          })
-  df = pd.DataFrame(data)
-  #printing all the details
-  print(df.to_string())
-  confrim_msg=input("Click yes to confirm or click No to modify the data:")
+  #printing the inputted details for confirmation
+    data = []
+    for subject, score in scores.items():
+        data.append({
+            'Student Name': s_name,
+            'Subject': subject,
+            'Score': score,
+            'Attendance (%)': attendance
+            })
+    df = pd.DataFrame(data)
+    
+    #printing all the details
+    print()
+    print(df.to_string())
+    
+    #confirming with the user
+    confrim_msg=input("\nType yes to confirm or no to re-enter your details:")
 
-  if confrim_msg=="yes":
-     print("Data confirmed and saved")
-     break
-  else:
-    print("Re-enter the data.") #user will re enter the details
-#after storing data we will calculate the progress
-print("\n")
-print("*"*30)
-print("Calculating your Progress....")   
+    if (confrim_msg.lower()=="yes"):
+      print("\nDetails Confirmed and Saved.\n")
+      #code for saving data in csv file
+      break
+    else:
+      print("\nRe-enter your Details.\n") #user will re enter the details
+      
+  #after storing data we will calculate the progress
+  print("*"*30)
+  print("Calculating your Progress....")   
+  
+  #calculate the progress
 
-#will display the progress
+  #will display the progress
